cbdfb103b095eee2b929de696f434fcde3e197e8
ilyas <ilyakoval2006@gmail.com>
2026-03-28 09:24:13 +0100

    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

---
+++ cmd/linux/cli/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ilyas-gg/laba5VSRPP/internal/pkg/app/cli"
+	"github.com/ilyas-gg/laba5VSRPP/pkg/logger"
+)
+
+func main() {
+	debugMode := os.Getenv("DEBUG") == "1"
+	appLogger := logger.New(debugMode)
+
+	app := cli.New(appLogger)
+	if err := app.Run(); err != nil {
+		appLogger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
