daf91859dd6de4426fe9022ebc28cd6b156c950d
ilyas <ilyakoval2006@gmail.com>
2026-04-04 08:52:23 +0200

    KEYFNqP7iSRU37cLf3pkbT3TNhLJ6Bwl+XP5VLig0s99GA/SnhrYjQ2acZJYUgTLjZxZXpdAEZZ2I2VidTLpFPMt5XroKbkuxdRbQO+QHiLb0A=KEY

--- cmd/linux/cli/main.go
+++ cmd/linux/cli/main.go
@@ -3,19 +3,42 @@ package main
 import (
 	"os"
 
+	"github.com/ilyas-gg/weather-app/internal/adapters/weather"
 	"github.com/ilyas-gg/weather-app/internal/pkg/app/cli"
+	"github.com/ilyas-gg/weather-app/internal/pkg/config"
 	"github.com/ilyas-gg/weather-app/pkg/logger"
 )
 
 func main() {
-	debugMode := os.Getenv("DEBUG") == "1"
-	appLogger := logger.New(debugMode)
-
-	app := cli.New(appLogger)
-	if err := app.Run(); err != nil {
-		appLogger.Error(err.Error())
-		os.Exit(1)
-	}
-
-	os.Exit(0)
+r, err := os.Open("./config/config.yaml")
+if err != nil {
+panic(err)
+}
+c, err := config.Parse(r)
+if err != nil {
+panic(err)
+}
+l := logger.New()
+wi := getProvider(c, l)
+app := cli.New(l, wi, c)
+err = app.Run()
+if err != nil {
+l.Error("Some error", err)
+os.Exit(1)
+}
+os.Exit(0)
+}
+func getProvider(c config.Config, l cli.Logger) cli.WeatherInfo {
+var wi cli.WeatherInfo
+switch c.P.Type {
+case "open-meteo":
+wi = weather.New(l)
+default:
+wi = weather.New(l)
+}
+return wi
+}
+r, err := os.Open("./config/config.yaml")
+if err != nil {
+panic(err)
 }
